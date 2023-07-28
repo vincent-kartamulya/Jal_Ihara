f8fba130054fd428dee27d6230cee8c119468ba9
vincent-kartamulya <vincent.learn01@gmail.com>
2023-07-28 09:17:52 +0700

    Starting Home

--- app/src/main/java/com/example/jal_ihara/HomeActivity.java
+++ app/src/main/java/com/example/jal_ihara/HomeActivity.java
@@ -8,18 +8,58 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class HomeActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private boolean isMenuVisible = false;
+    private View slidingMenu;
+    private TextView menuItem1;
+    private TextView menuItem2;
+    private TextView menuItem3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        TextView viewUsername = findViewById(R.id.username);
 
-        Intent fromLogin = getIntent();
-        viewUsername.setText(fromLogin.getStringExtra("username"));
+        slidingMenu = findViewById(R.id.slidingMenuLayout);
+        menuItem1 = findViewById(R.id.menu1);
+        menuItem2 = findViewById(R.id.menu2);
+        menuItem3 = findViewById(R.id.menu3);
+
+        ImageView burger = findViewById(R.id.burger);
+        TextView logo = findViewById(R.id.logo);
+
+//        Intent fromLogin = getIntent();
+//        viewUsername.setText(fromLogin.getStringExtra("username"));
+
+        burger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleMenu();
+            }
+        });
+
+        menuItem1.setOnClickListener(this);
+        menuItem2.setOnClickListener(this);
+        menuItem3.setOnClickListener(this);
+    }
+
+    private void toggleMenu() {
+        if (slidingMenu.getVisibility() == View.VISIBLE) {
+            slidingMenu.setVisibility(View.GONE);
+        } else {
+            Animation slideDownAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_down);
+            slidingMenu.startAnimation(slideDownAnimation);
+            slidingMenu.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -30,10 +70,38 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 //    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        int id = item.getItemId();
-//        switch (id) {
-//            case
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle item selection for options menu here
+//        switch (item.getItemId()) {
+//            case R.id.option1:
+//                // Handle option 1 click
+//                return true;
+//            case R.id.option2:
+//                // Handle option 2 click
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
 //        }
 //    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.menu1:
+                showToast("Clicked: Menu 1");
+                break;
+            case R.id.menu2:
+                showToast("Clicked: Menu 2");
+                break;
+            case R.id.menu3:
+                showToast("Clicked: Menu 3");
+                break;
+        }
+        toggleMenu();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 }
+
